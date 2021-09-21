ff96751b947939a240a725ac9620ce06597a895d
Joey Nelson <joey@joescan.com>
2021-09-21 11:07:52 -0700

    Initial checkin

--- src/MathPlay.jl
+++ src/MathPlay.jl
@@ -2,4 +2,15 @@ module MathPlay
 
 # Write your package code here.
 
+export colatz
+
+function colatz(x,n)
+    res = BitVector(undef,n)
+    for i = 1:n
+        res[i] = isodd(x)
+        x = iseven(x) ? x ÷ 2 : (3x + 1) ÷ 2
+    end
+    return res
+end
+
 end
